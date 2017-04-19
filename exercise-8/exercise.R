bd62dc114ba7d6857a41b0a13a19db7cab94bd82
Charles Mihran <cmihran@cs.washington.edu>
2017-04-19 14:14:54 -0700

    exercise 8

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -1,6 +1,7 @@
 # Exercise 8: Pulitzer Prizes
 
 # Read in the data
+setwd("~/Documents/School/INFO201/m11-dplyr/exercise-8/")
 pulitzer <- read.csv("data/pulitzer-circulation-data.csv", stringsAsFactors = FALSE)
 
 # Install and load the needed libraries
@@ -11,21 +12,22 @@ library(dplyr)
 
 # View in the data set. Start to understand what the data columns contains
 # Be sure to comment out the function so it won't view everytime you run the code.
-
+View(pulitzer)
 
 # Use 'colnames' to print out the names of the columns
-
+colnames(pulitzer)
 
 # Use 'str' to print what types of values are contained in each column
 # Did any value type surprise you? Why do you think they are that type?
-
+print(str(pulitzer))
 
 # Add a column in a dataframe called 'Pulitzer.Prize.Change` that contains the diffrence in changes
 # in Pulitzer Prize Winners from 2004 to 2013 and Pultizer Prize Winners from 1990 to 2003.
-
+Pulitzer.Prize.Change <- mutate(pulitzer, diff.2004.2014.to.1990.2003 = Pulitzer.Prize.Winners.and.Finalists..2004.2014 - Pulitzer.Prize.Winners.and.Finalists..1990.2003)
 
 # What publication gained the most pulitzer prizes from 2004-2014?
 # Be sure to use the pipe operator! 
+most.2014 <- filter(pulitzer.difference, max(Pulitzer.Prize.Winners.and.Finalists..2004.2014) == Pulitzer.Prize.Winners.and.Finalists..2004.2014) %>% select(Newspaper)
 
 
 # Which publication with at least 5 Pulitzers won from 2004-2014 had the biggest decrease(negative) in Daily circulation numbers? 
